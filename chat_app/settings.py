71c08946c1934284637ef0a7fdc9a21324a3f326
pnBafon <bafonprecious@gmail.com>
2024-04-10 08:02:06 +0100

    udated the settings

--- chat_app/settings.py
+++ chat_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-37&=ovgv219+w9u-$e5hxirpip!$7@%=^#*@#u(2i5%!=l!63o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
